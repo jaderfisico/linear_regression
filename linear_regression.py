bd83c8ff77bf34255fcc45a6b200c3e4d720e2f0
jaderson <jaderfisico@gmail.com>
2017-12-11 19:09:06 -0200

    This version of the file includes the uncertainties mass of the BH

--- linear_regression.py
+++ linear_regression.py
@@ -12,14 +12,17 @@ sigma = np.loadtxt(name_file, usecols=(7,))
 MBH = np.log10(mbh)
 SIGMA = np.log10(sigma/200)
 
-#Here we import the uncertainties in in both variables: MBH and SIGMA 
+#here we import the uncertainties in the data
 
 plt.scatter(SIGMA, MBH)
 plt.show()
 
 #now we define the priors (alpha, beta). In this case we choose uniform priors
 
-alpha =  pymc.Uniform('alpha', lower=-5, upper=5)
+mbh_er = np.loadtxt(name_file, usecols=(4,))
+MBH_ER = (1./mbh)*(np.abs(mbh - mbh_er))
+
+alpha =  pymc.Uniform('alpha', lower=-10, upper=10)
 beta = pymc.Uniform('beta', lower=-10, upper=10)
 
 #Finally, we define our observations
@@ -30,10 +33,34 @@ x = pymc.Normal('x', mu=0, tau=1, value=SIGMA, observed=True)
 def linear_regress(x=x, alpha=alpha, beta=beta):
     return x*alpha + beta
 
-y = pymc.Normal('output',mu=linear_regress, value=MBH, observed=True)
+#y = pymc.Normal('output',mu=linear_regress,\
+#        value=MBH, observed=True)
+
+y = pymc.Normal('output',mu=linear_regress, tau=np.power(MBH_ER,-2),\
+        value=MBH, observed=True)
 
 
 model = pymc.Model([x, y, alpha, beta])
 mcmc = pymc.MCMC(model)
 mcmc.sample(iter=10000,burn=1000, thin=10)
+
+#Show the results
+print alpha.value, np.std(alpha.trace())
+print beta.value, np.std(beta.trace())
+
+plt.scatter(alpha.trace(), beta.trace())
+
+plt.show()
+
+#Now plot the results
+
+for a,b in np.c_[alpha.trace(), beta.trace()]:
+    plt.plot(SIGMA, a*SIGMA + b, 'red', alpha=0.01)
+
+#plt.errorbar(SIGMA,MBH, yerr=MBH_ER)
+#plt.scatter(SIGMA, MBH)
+plt.errorbar(SIGMA, MBH, yerr=MBH_ER, fmt='bo')
+
+plt.show()
+
 
